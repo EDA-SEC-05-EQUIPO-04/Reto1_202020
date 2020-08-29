757d87e817ae10af3e728adaf4173703ba19c3fc
Jaime Andres Torres Bermejo <j.torres16@uniandes.edu.co>
2020-08-28 19:43:45 -0500

    añadida documentación

--- App/funcionreto.py
+++ App/funcionreto.py
@@ -3,6 +3,13 @@ def MovieSorting (list1):
     FUNCION 2:
     list1: Lista de metadatos de la película.
 
-    
+    Como aficionado del cine Quiero crear el Ranking  de las películas  
+    por las mas y menos votadas (COUNT) y por las mejor o peor calificadas 
+    (AVERAGE)  del catalogo.
+
+    Las condicionesson:−Por lo menos 10 películas (x = 10).
+    −COUNT se ordena por la cantidad de votos (vote_count).
+    −AVERAGE se ordena por la calificación promedio (vote_average).
+    −Ordenamiento ascendente y descendente.
     """
     
